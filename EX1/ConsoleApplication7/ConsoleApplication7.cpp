5d757f1764693e2daa91196f1fe5107b152c5a53
kiks27 <80412065+kiks27@users.noreply.github.com>
2021-09-21 11:06:42 +0200

    second commit

--- EX1/ConsoleApplication7/ConsoleApplication7.cpp
+++ EX1/ConsoleApplication7/ConsoleApplication7.cpp
@@ -4,13 +4,16 @@
 #include <conio.h>
 int main()
 {
+	printf("Entrez 2 nombres : \n");
 	int a = 0;
 	int b = 0;
 	int produit = 0;
 	int r = 0;
 
 
-	scanf("%d", &a);	scanf("%d", &b);
+	scanf("%d", &a);
+	scanf("%d", &b);
+
 	produit = produit + b;
 
 	while (produit > 0) {
